71aa0ac3f5f36fc5373211716055c365b5124eeb
Jun Shimura <junshimura@gmail.com>
2021-04-18 20:36:16 +0900

    プロジェクト ファイルを追加します。

---
+++ NetTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DisplayLocalHostName();
+            Console.ReadKey();
+        }
+        static public void DisplayLocalHostName()
+        {
+            try {
+                // Get the local computer host name.
+                String hostName = Dns.GetHostName();
+                Console.WriteLine("Computer name :" + hostName);
+            }
+            catch (SocketException e) {
+                Console.WriteLine("SocketException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+        }
+
+    }
+}
