067622a65392431a291dcf92925a986513d716c4
Jun Shimura <junshimura@gmail.com>
2021-06-24 11:58:04 +0900

    ChatSystem

---
+++ ChatSystem/ChatSystem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatSystem
+{
+    class ChatSystem
+    {
+        private string _hostName;
+        public string hostName
+        {
+            get => _hostName;
+        }
+        public enum ConnectMode { host = 0, client = 1 };
+        private ConnectMode _connectMode;
+        public ConnectMode connectMode
+        {
+            get => _connectMode;
+        }
+        public ChatSystem()
+        {
+            _hostName = Dns.GetHostName();
+        }
+        private IPAddress _ipAddress;
+        private Int32 _portNo;
+
+        private IPEndPoint _localEndPoint;
+
+        public void SetConnectMode(ConnectMode connectMode)
+        {
+            _connectMode = connectMode;
+        }
+
+        public void InitializeHost(IPAddress ipAddress,Int32 portNo )
+        {
+            _connectMode = ConnectMode.host;
+            _ipAddress = ipAddress;
+            _portNo = portNo;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, portNo);
+            //ソケットの作成
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //通信の受け入れ準備
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            //通信の確立
+            Socket handler = listener.Accept();
+        }
+        public bool InitializeClient(IPAddress ipAddress, Int32 portNo)
+        {
+            _connectMode = ConnectMode.client;
+            _ipAddress = ipAddress;
+            _portNo = portNo;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, portNo);
+            //ソケットを作成
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //接続する。失敗するとエラーで落ちる。
+            try
+            {
+                socket.Connect(remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Connect Faild{e.ToString()}");
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
---
+++ ChatSystem/main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatSystem
+{
+    class main
+    {
+        static ChatSystem chatSystem;
+        const Int32 portNo = 11000;
+
+        static void Main(string[] args)
+        {
+            chatSystem = new ChatSystem();
+            Console.WriteLine($"this hostName is {chatSystem.hostName}.");
+            ChatSystem.ConnectMode connectMode= SelectMode();
+            chatSystem.SetConnectMode(connectMode);
+
+        }
+        static ChatSystem.ConnectMode  SelectMode()
+        {
+            ChatSystem.ConnectMode connectMode= ChatSystem.ConnectMode.host;
+            Console.Write("Select Mode:\n0=Host,1=Client");
+            int select = int.Parse(Console.ReadLine());
+            switch (select)
+            {
+                case 0: //Host
+                    connectMode = ChatSystem.ConnectMode.host;
+                    Console.WriteLine("Running Host mode");
+                    InitializeHost();
+                    break;
+                case 1: //Client
+                    connectMode = ChatSystem.ConnectMode.client;
+                    Console.WriteLine("Running Client mode");
+                    InitializeClient();
+                    break;
+                default:
+                    Console.WriteLine("ERROR undefind");
+                    break;
+            }
+            return connectMode;
+        }
+        static void InitializeHost()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(chatSystem.hostName);
+            foreach (var addresslist in ipHostInfo.AddressList)
+            {
+                Console.WriteLine($"found own address:{addresslist.ToString()}");
+            }
+            Console.Write($"Select address to listen(0 - {ipHostInfo.AddressList.Length - 1}):");
+            IPAddress ipAddress = ipHostInfo.AddressList[int.Parse(Console.ReadLine())];
+            chatSystem.InitializeHost(ipAddress, portNo);
+        }
+        static void InitializeClient()
+        {
+            Console.Write("Input IP address to connect:");
+            var ipAddress = IPAddress.Parse(Console.ReadLine());
+            bool suceed =chatSystem.InitializeClient(ipAddress, 11000);
+            if (suceed)
+            {
+                Console.WriteLine($"Connected host {ipAddress.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine("faled to connect to host");
+            }
+        }
+    }
+}
